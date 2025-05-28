2b769fbf8eb13dd790a510b84c980f1d7ec498a9
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2025-05-28 11:47:21 +0100

    add files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # mrs
+
+This is a small command line tool that checks your open MRs on a given project in gitlab.
+
+```bash
+[oscarsaharoy@LCCC-MB-pTIY21 ~/projects/mrs] $ mrs
+You have 6 open MRs.
+!2424 [need_rebase    ] ZRO-3760: address unique together fix
+!2423 [not_approved   ] ZRO-3556: fix contract assignment search
+!2421 [unchecked      ] ZRO-3194: lcha register flags
+!2409 [not_approved   ] ZRO-3594: KYC Cleanup
+!2370 [not_approved   ] Feature/zro 3622 at function frontend
+!2245 [unchecked      ] ZRO-1: add extra logging and retries for pdf generation
+```
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use reqwest::Client;
+use serde::Deserialize;
+use dotenvy::dotenv;
+use std::env;
+use std::path::Path;
+
+#[derive(Deserialize, Debug)]
+struct MergeRequest {
+    iid: u64,
+    title: String,
+    detailed_merge_status: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let author_username = "oscarsaharoylccc";
+    let dotenv_path = "/Users/oscarsaharoy/projects/mrs/.env";
+
+    dotenvy::from_path(Path::new(dotenv_path)).ok();
+    let gitlab_url = "https://gitlab.com/api/v4/merge_requests";
+    let token = env::var("GITLAB_TOKEN").expect("GITLAB_TOKEN must be set in .env");
+
+    let client = Client::new();
+    let response = client
+        .get(gitlab_url)
+        .query(&[
+            ("scope", "all"),
+            ("state", "opened"),
+            ("author_username", author_username),
+            ("per_page", "100"),
+        ])
+        .header("PRIVATE-TOKEN", token)
+        .send()
+        .await?
+        .json::<Vec<MergeRequest>>()
+        .await?;
+
+    println!("You have {} open MRs.", response.len());
+    for mr in response {
+        println!("!{} [{}] {}", mr.iid, format!("{:15}", mr.detailed_merge_status), mr.title);
+    }
+
+    Ok(())
+}
+
